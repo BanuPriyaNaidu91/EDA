14546d617b81509fd65d1ed07d16fb356add2ed9
BanuPriyaNaidu91 <41759248+BanuPriyaNaidu91@users.noreply.github.com>
2018-09-23 19:20:47 +0530

    Create EPC.R

---
+++ EPC.R
@@ -0,0 +1,46 @@
+
+#Read File
+files <- file('.EA/household_power_consumption.txt')
+
+#Read data based on date and when its not '?'
+data <- read.table(text = grep("^[1,2]/2/2007",readLines(files),value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')  
+
+#Plot 1
+#Open device
+png(filename = '.EA/plot1.png', width = 480, height = 480, units='px')
+with(data, hist(Global_active_power, xlab = 'Global Active Power (kilowatt)', main = 'Global Active Power', col = 'red'))
+dev.off()
+
+
+#Plot 2
+data$Date <- as.Date(data$Date, format = '%d/%m/%Y')
+data$DateTime <- as.POSIXct(paste(data$Date, data$Time))
+png(filename = 'C:/Users/b.s.priya/Desktop/Data Science/EA/household_power_consumption/plot2.png', width = 480, height = 480, units='px')
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+dev.off()
+
+#Plot 3
+png(filename = '.EA/plot3.png', width = 480, height = 480, units='px')
+Sys.setlocale(category = "LC_ALL", locale = "english")
+plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+dev.off()
+
+#Plot 4
+png(filename = '.EA/plot4.png', width = 480, height = 480, units='px')
+Sys.setlocale(category = "LC_ALL", locale = "english")
+par(mfrow = c(2, 2))
+plot(data$DateTime, data$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
+plot(data$DateTime, data$Voltage, xlab = 'datetime', ylab = 'Voltage', type = 'l')
+plot(data$DateTime, data$Sub_metering_1, xlab = '', ylab = 'Energy sub metering', type = 'l')
+lines(data$DateTime, data$Sub_metering_2, col = 'red')
+lines(data$DateTime, data$Sub_metering_3, col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd = 1)
+plot(data$DateTime, data$Global_reactive_power, xlab = 'datetime', ylab = 'Global_reactive_power', type = 'l')
+dev.off()
+
+
+
